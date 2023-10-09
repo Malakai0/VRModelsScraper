f384e7d39030d4104b6fbfb595fc3f3cb8f3e016
Malakai <41552723+Malakai0@users.noreply.github.com>
2023-10-09 11:21:14 +0100

    --update flag

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import server from "./mod/jobs/server.js";
 
 scraper.init();
 
-new Promise((resolve) => {
-  resolve(scraper.start());
-});
+/* If ran with the --update flag, update the database */
+if (process.argv.includes("--update")) {
+  scraper.update();
+} else {
+  new Promise((resolve) => {
+    resolve(scraper.start());
+  });
 
-new Promise((resolve) => {
-  resolve(server());
-});
+  new Promise((resolve) => {
+    resolve(server());
+  });
+}
